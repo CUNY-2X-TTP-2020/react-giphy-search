be5e3150e6c168646c4a5477a31da6930a95ab05
RickyC0626 <rickychon99@gmail.com>
2020-06-05 10:53:53 -0400

    Add app title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App()
     return (
         <div className="App">
             <header className="App-header">
-                
+                <h1>Giphy Search</h1>
             </header>
         </div>
     );
