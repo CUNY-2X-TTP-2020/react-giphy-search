016e902d9da24edc0de81a52b6341ac506e9c146
RickyC0626 <rickychon99@gmail.com>
2020-06-05 16:30:22 -0400

    Refactor search bar for organization

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,8 +10,19 @@ export default class SearchBar extends Component
         this.state =
         {
             prevSearch: "Enter a topic to search for",
-            hasSearched: false
+            hasSearched: false,
+            isRandom: false,
+            isTrending: true
         }
+    }
+
+    handleButtonClick = (event) =>
+    {
+        // Prevent browser reload/refresh
+        event.preventDefault();
+
+        if(event.target.id === "random") this.setState({ isRandom: true, isTrending: false });
+        else if(event.target.id === "trending") this.setState({ isRandom: false, isTrending: true });
     }
 
     handleSubmit = (event) =>
@@ -19,29 +30,62 @@ export default class SearchBar extends Component
         // Prevent browser reload/refresh
         event.preventDefault();
 
-        this.setState({ prevSearch: event.target.searchbar.value, hasSearched: true })
+        this.setState(
+        { 
+            prevSearch: event.target.searchbar.value, 
+            hasSearched: true,
+            isRandom: false,
+            isTrending: false
+        });
         event.target.reset();
     }
 
     render()
     {
+
         return (
             this.state.hasSearched ?
             <section>
-                <form onSubmit={this.handleSubmit}>
-                    <div>Gif Search</div>
-                    <input type="text" name="searchbar" id="searchbar" placeholder={this.state.prevSearch} />
-                </form>
-                <GifFetcher searchTerm={this.state.prevSearch} searchType="regular" />
+                {this.getSearchBar()}
+                {this.getButtons()}
+                {this.getGifFetcher()}
             </section>
             :
             <section>
-                <form onSubmit={this.handleSubmit}>
-                    <div>Gif Search</div>
-                    <input type="text" name="searchbar" id="searchbar" placeholder={this.state.prevSearch} />
-                </form>
+                {this.getSearchBar()}
+                {this.getButtons()}
                 <GifFetcher />
             </section>
         );
+    }
+
+    getSearchBar()
+    {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>Gif Search</div>
+                <input type="text" name="searchbar" id="searchbar" placeholder={this.state.prevSearch} />
+            </form>
+        );
+    }
+
+    getButtons()
+    {
+        return (
+            <section>
+                <button onClick={this.handleButtonClick} id="random">Random</button>
+                <button onClick={this.handleButtonClick} id="trending">Trending</button>
+            </section>
+        );
+    }
+
+    getGifFetcher()
+    {
+        const isRandom = this.state.isRandom;
+        const isTrending = this.state.isTrending;
+
+        if(isRandom && !isTrending) return <GifFetcher searchType="random" />
+        else if(!isRandom && isTrending) return <GifFetcher searchType="trending" />
+        else if(!isRandom && !isTrending) return <GifFetcher searchTerm={this.state.prevSearch} searchType="regular" />
     }
 }
